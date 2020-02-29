119fccf5a95b01d65e15f7a754fe984e68f1d8b5
Alkl58 <34812414+Alkl58@users.noreply.github.com>
2020-02-29 15:17:49 +0100

    Added Reencode to Lossless before encoding option

--- NotEnoughEncodes/MainWindow.xaml.cs
+++ NotEnoughEncodes/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Win32;
 using System.IO;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Path = System.IO.Path;
 using System.Threading;
 using System.Diagnostics;
-using Timer = System.Timers.Timer;
 using System.Windows.Threading;
 
 namespace NotEnoughEncodes
@@ -72,8 +62,18 @@ namespace NotEnoughEncodes
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
             //FFmpeg Arguments
-            startInfo.Arguments = "/C ffmpeg.exe -i " + '\u0022' + videoInput + '\u0022' + " -vcodec copy -f segment -segment_time " + TextBoxChunkLength.Text + " -an " + '\u0022' + "Chunks\\out%0d.mp4" + '\u0022';
-            Console.WriteLine(startInfo.Arguments);
+
+            //Checks if Source needs to be reencoded
+            if (CheckBoxReencode.IsChecked == false)
+            {
+                startInfo.Arguments = "/C ffmpeg.exe -i " + '\u0022' + videoInput + '\u0022' + " -vcodec copy -f segment -segment_time " + TextBoxChunkLength.Text + " -an " + '\u0022' + "Chunks\\out%0d.mp4" + '\u0022';
+
+            }else if (CheckBoxReencode.IsChecked == true)
+            {
+                startInfo.Arguments = "/C ffmpeg.exe -i " + '\u0022' + videoInput + '\u0022' + " -c:v utvideo -f segment -segment_time " + TextBoxChunkLength.Text + " -an " + '\u0022' + "Chunks\\out%0d.mkv" + '\u0022';
+
+            }            
+            //Console.WriteLine(startInfo.Arguments);
             process.StartInfo = startInfo;
             process.Start();
             process.WaitForExit();
@@ -88,16 +88,34 @@ namespace NotEnoughEncodes
             //Checks if there is more than 9 Chunks, inorder to rename them, because later in order to concat, it has to be sortet (Values from xx-xx instead of x-xx)
             if (chunks.Count() >= 10)
             {
-                System.IO.File.Move(sdira + "\\out0.mp4", sdira + "\\out00.mp4");
-                System.IO.File.Move(sdira + "\\out1.mp4", sdira + "\\out01.mp4");
-                System.IO.File.Move(sdira + "\\out2.mp4", sdira + "\\out02.mp4");
-                System.IO.File.Move(sdira + "\\out3.mp4", sdira + "\\out03.mp4");
-                System.IO.File.Move(sdira + "\\out4.mp4", sdira + "\\out04.mp4");
-                System.IO.File.Move(sdira + "\\out5.mp4", sdira + "\\out05.mp4");
-                System.IO.File.Move(sdira + "\\out6.mp4", sdira + "\\out06.mp4");
-                System.IO.File.Move(sdira + "\\out7.mp4", sdira + "\\out07.mp4");
-                System.IO.File.Move(sdira + "\\out8.mp4", sdira + "\\out08.mp4");
-                System.IO.File.Move(sdira + "\\out9.mp4", sdira + "\\out09.mp4");
+                if (CheckBoxReencode.IsChecked == false)
+                {
+                    System.IO.File.Move(sdira + "\\out0.mp4", sdira + "\\out00.mp4");
+                    System.IO.File.Move(sdira + "\\out1.mp4", sdira + "\\out01.mp4");
+                    System.IO.File.Move(sdira + "\\out2.mp4", sdira + "\\out02.mp4");
+                    System.IO.File.Move(sdira + "\\out3.mp4", sdira + "\\out03.mp4");
+                    System.IO.File.Move(sdira + "\\out4.mp4", sdira + "\\out04.mp4");
+                    System.IO.File.Move(sdira + "\\out5.mp4", sdira + "\\out05.mp4");
+                    System.IO.File.Move(sdira + "\\out6.mp4", sdira + "\\out06.mp4");
+                    System.IO.File.Move(sdira + "\\out7.mp4", sdira + "\\out07.mp4");
+                    System.IO.File.Move(sdira + "\\out8.mp4", sdira + "\\out08.mp4");
+                    System.IO.File.Move(sdira + "\\out9.mp4", sdira + "\\out09.mp4");
+                }
+                else if (CheckBoxReencode.IsChecked == true)
+                {
+                    System.IO.File.Move(sdira + "\\out0.mkv", sdira + "\\out00.mkv");
+                    System.IO.File.Move(sdira + "\\out1.mkv", sdira + "\\out01.mkv");
+                    System.IO.File.Move(sdira + "\\out2.mkv", sdira + "\\out02.mkv");
+                    System.IO.File.Move(sdira + "\\out3.mkv", sdira + "\\out03.mkv");
+                    System.IO.File.Move(sdira + "\\out4.mkv", sdira + "\\out04.mkv");
+                    System.IO.File.Move(sdira + "\\out5.mkv", sdira + "\\out05.mkv");
+                    System.IO.File.Move(sdira + "\\out6.mkv", sdira + "\\out06.mkv");
+                    System.IO.File.Move(sdira + "\\out7.mkv", sdira + "\\out07.mkv");
+                    System.IO.File.Move(sdira + "\\out8.mkv", sdira + "\\out08.mkv");
+                    System.IO.File.Move(sdira + "\\out9.mkv", sdira + "\\out09.mkv");
+
+                }
+
                 //Clears the Array
                 Array.Clear(chunks, 0, chunks.Length);
                 //Reads all Files to array
@@ -189,7 +207,7 @@ namespace NotEnoughEncodes
                                 startInfo.FileName = "cmd.exe";
                                 startInfo.Arguments = "/C ffmpeg.exe -i " + '\u0022' + sdira + "\\" + items + '\u0022' + " -pix_fmt yuv420p -vsync 0 -f yuv4mpegpipe - | aomenc.exe - --passes=1 --cpu-used=" + cpuUsed + " --threads=" + encThreads + finalEncodeMode + " --bit-depth=" + bitDepth + " --tile-columns=" + tilecols + " --fps=" + fps + " --tile-rows=" + tilerows + " --kf-max-dist=" + kfmaxdist + " --output=Chunks\\" + items + "-av1.ivf";
                                 process.StartInfo = startInfo;
-                                Console.WriteLine(startInfo.Arguments);
+                                //Console.WriteLine(startInfo.Arguments);
                                 process.Start();
                                 process.WaitForExit();
                                 //Progressbar +1
@@ -248,10 +266,6 @@ namespace NotEnoughEncodes
 
             pLabel.Dispatcher.Invoke(() => pLabel.Content = "Muxing files", DispatcherPriority.Background);
 
-            //Creates Output Folder
-            if (!Directory.Exists(Path.Combine(currentPath, "Output")))
-                Directory.CreateDirectory(Path.Combine(currentPath, "Output"));
-
             //Lists all ivf files in mylist.txt
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
@@ -268,7 +282,7 @@ namespace NotEnoughEncodes
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
             //FFmpeg Arguments
-            startInfo.Arguments = "/C ffmpeg.exe -f concat -safe 0 -i Chunks\\mylist.txt -c copy Output\\output.mkv";
+            startInfo.Arguments = "/C ffmpeg.exe -f concat -safe 0 -i Chunks\\mylist.txt -c copy output.mkv";
             //Console.WriteLine(startInfo.Arguments);
             process.StartInfo = startInfo;
             process.Start();
@@ -303,5 +317,6 @@ namespace NotEnoughEncodes
             }
             catch { }
         }
+
     }
 }
